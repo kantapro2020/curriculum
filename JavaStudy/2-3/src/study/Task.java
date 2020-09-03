59dff848f519b53ac17f35cd44748cf1dbaf6fce
kantapro2020 <kantapro2020@gmail.com>
2020-09-03 19:52:12 +0900

    ２章　パッケージ
    2-4作成

--- JavaStudy/2-3/src/study/Task.java
+++ JavaStudy/2-3/src/study/Task.java
@@ -9,7 +9,7 @@ package study;
         // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
     	int a = plus(10);
     	System.out.println("plusメソッドの引数が一つの場合：" + a);
-    	int b = plus(5, 20);
+    	int b = plus(10, 20);
     	System.out.println("plusメソッドの引数が一つの場合：" + b);
     	int c = plus(10, 20, 30);
     	System.out.println("plusメソッドの引数が一つの場合：" + c);
