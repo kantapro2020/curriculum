9bc349c7d4a0f56e1db34d3cb15bbd74cc1762b9
kantapro2020 <kantapro2020@gmail.com>
2020-09-12 18:28:32 +0900

    2-4修正

--- JavaStudy/2-4/src/fruits/Fruits.java
+++ JavaStudy/2-4/src/fruits/Fruits.java
@@ -1,8 +1,5 @@
 package fruits;
 
 public class Fruits {
-	public static void printFruits(String fruits, int count) {
-        System.out.println("食べ物は" + fruits);
-        System.out.println(count + "個です");
-    }
+
 }
