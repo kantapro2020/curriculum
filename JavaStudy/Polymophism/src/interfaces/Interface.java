8bec898753662d0c61ac4fc30f223a9a8cbbcab3
kantapro2020 <kantapro2020@gmail.com>
2020-09-17 22:08:44 +0900

    ４章　例外処理
    4-1作成

---
+++ JavaStudy/Polymophism/src/interfaces/Interface.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+public interface Interface {
+	/* ----- インターフェースメソッドの {} の処理は何も書かない！ ----- */
+    // 司令（月末までに勤務表出せや！
+	public void daseyoKinmuhyo();
+	// 司令（できれば月末までに交通費出せや！
+	public void daseyoKotsuhi();
+	// お邪魔虫な司令
+    // 例）セブンイレブン行けや！
+    // （こじつけてしまえば何でも有りですが、必ずこなす業務的な司令としては、なんか適していないよねって話
+	public void goToSevenEleven();
+	void daseyaKinmuhyo();
+}
